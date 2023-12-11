c1ef958c69f11b6a74fb33cb391f1f1d86247d12
L344S <7474@holbertonstudents.com>
2023-12-11 13:45:06 +0100

    Added header.h files that contains libraries and functions prototypes

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* NewShell.c -> printing a prompt */
+int main(int argc, char *argv);
+/* read_line.c -> reading the user input */
+char *read_line(void);
+
+
+#endif
