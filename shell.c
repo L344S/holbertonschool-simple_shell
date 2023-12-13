65adc98f8fba00243e097861928b7c2045b34962
yazgahar <battianas@hotmail.com>
2023-12-13 17:39:39 +0100

    modified new functions

--- shell.c
+++ shell.c
@@ -1,25 +1,27 @@
 #include "shell.h"
 /**
-* main - entry point
-* Return: Always 0 on success
-*/
+ * main - entry point
+ * Return: Always 0 on success
+ */
 int main(void)
 {
-	const char *path = ".";  /** Default to current directory */
+	int i;
+	char *line;
+	char **tokens;
 
 	while (1)
 	{
-	/* Print the prompt */
-	custom_prompt();
-	/* Read user input */
-	read_line();
-	/* execute the command */
+		custom_prompt();
+		line = read_line();
 
-    /** Call the function with the specified path */
-    listFilesInDirectory(path);
-	
-	listFiles(path);
+		tokens = tokenize_line(line);
+
+		for (i = 0; tokens[i] != NULL; i++)
+		{
+			free(tokens[i]);
+		}
+		free(tokens);
+		free(line);
 	}
-
 	return (0);
 }
