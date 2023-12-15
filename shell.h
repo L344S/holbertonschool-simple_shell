977b2ad4d9e5b01f6a1daff5c48c50345c96a4f5
L344S <7474@holbertonstudents.com>
2023-12-15 04:46:05 +0100

    Updated shell.h file with new prototypes, macros and libraries
    Code has been verified using the Betty style guide and is functioning correctly in the local environment.

--- shell.h
+++ shell.h
@@ -5,24 +5,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <dirent.h>
 #include <unistd.h>
 #include <string.h>
-#include <dirent.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 
 /* ----------Macros---------- */
 extern char **environ;
 #define BUFSIZE 1024
+#define SPACE ' '
+#define TAB '\t'
 
 /* ----------Prototypes---------- */
 /* shell.c */
 int printPrompt(void);
-/* read_line.c -> reading the user input */
-char *read_line(void);
+/* ReadLine.c */
+char *readLine(void);
+/* parseLine.c */
+char **parseLine(char *line);
+/* handle_input.c */
+int emptyLine(char *line);
+/* execute.c */
+int execute(char **args);
+/* get_env.c */
+char *_getenv(char *name);
+/* get_path.c */
+char *get_path(char *file);
+/* free2.c */
+void freeDP(char **args);
+/* free2.c */
+void freeMemory(char **tokens, size_t count);
 
 void listFilesInDirectory(const char *path);
-
 void listFiles(const char *path);
 
 #endif
