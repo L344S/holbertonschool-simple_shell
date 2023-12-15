833a2d53d5a80c307e790c3d49d9f039bd8e27ce
L344S <7474@holbertonstudents.com>
2023-12-15 04:48:39 +0100

    Updated shell.c file
    Code has been verified using the Betty style guide and is functioning correctly in the local environment.

--- shell.c
+++ shell.c
@@ -1,20 +1,46 @@
 #include "shell.h"
+
 /**
- * main - entry point
- * Return: Always 0 on success
- */
+* main - entry point of the program
+* Description: run the shell program
+* Only if stdin is associated with a terminal
+* Print the prompt, get the input, parse the input line, execute the command
+* Return: 0 on success or a number on failure
+*/
 int main(void)
 {
-	/* variables */
-	const char *path = ".";  /* Default to current directory */
+	char *input_line = NULL; /* Store the user input line */
+	char **command_arguments; /* Store array of commands arguments */
 
-	while (1)
+	/* Infinite loop to run the shell */
+	while (1) /* 1 is true */
 	{
-		printPrompt();
-		read_line();
+		printPrompt(); /* Print the custom prompt of our shell */
+		input_line = readLine(); /* Get the input line from the user */
 
-	/* Call the function with the specified path */
-	listFiles(path);
+		/* Check if emptyLine is filled with spaces or tabs */
+		if (emptyLine(input_line) == 1) /* 1 is true */
+		{
+			continue; /* Go back to the beginning of the loop (printPrompt again) */
+		}
+		/* Parse the input line and store them in command_arguments array*/
+		command_arguments = parseLine(input_line);
+
+		/* Check if the user wants to exit the shell */
+		if (strcmp(command_arguments[0], "exit") == 0) /* compare if arg0 == exit */
+			break; /* if true, exit the shell */
+
+		/* Check if the user input is a not a built-in command */
+		if (execute(command_arguments) == -1)
+		{
+			/* Print error message */
+			fprintf(stderr, "hsh: command not found: %s\n", command_arguments[0]);
+			continue; /* Go back to the beginning of the loop (printPrompt again) */
+		}
+		freeDP(command_arguments); /* Free the command_arguments array */
+		command_arguments = NULL; /* Set command_arguments to NULL */
 	}
-	return (0);
+	free(input_line); /* Free the input_line */
+
+	return (0); /* Return 0 on success */
 }
