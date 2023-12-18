77034d6ab6f38e648c76d7de1cba90c43507e3c6
yazgahar <battianas@hotmail.com>
2023-12-18 14:15:26 +0100

    handle memory properly

--- readLine.c
+++ readLine.c
@@ -1,34 +1,35 @@
 #include "shell.h"
 
 /**
-* readLine - read a line from stdin
-* Description:
-    * Use getline function to get the input user from stdin
-	* Getline will dynamically allocate memory and update input_line
-	* Getline return the number of characters read and store it in nb_char_line
-* Return: the line on success or NULL on failure
-*/
+ * readLine - read a line from stdin
+ * Description:
+ * Use getline function to get the input user from stdin
+ * Getline will dynamically allocate memory and update input_line
+ * Getline return the number of characters read and store it in nb_char_line
+ * Return: the line on success or NULL on failure
+ */
 char *readLine(void)
 {
-    /* Initialize the line */
 	char *input_line = NULL;
-    /* Initialize the size of the line */
 	size_t size_line = 0;
-    /* Initialize the number of characters read */
 	ssize_t read;
 
-	/* Read the line from stdin */
 	read = getline(&input_line, &size_line, stdin);
-	/* If getline fails */
-	if (read < 0 || !read)
+
+	if (read == -1)
 	{
-		/* Print error message */
-		perror("Failed to read the command\n");
-		/* Free the line */
-		free(input_line);
-		/* Return NULL */
-		return (NULL);
+		if (feof(stdin))
+		{
+			printf("Exiting the shell.\n");
+			free(input_line);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("Failed to read the command\n");
+			free(input_line);
+			return (NULL);
+		}
 	}
-	/* Return the line */
 	return (input_line);
 }
