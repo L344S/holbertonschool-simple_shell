2923ba9f6d3a62e6e35d4ac1a4db573cc099b9f9
yazgahar <battianas@hotmail.com>
2023-12-18 19:19:16 +0100

    handle memory exit properly

--- shell.c
+++ shell.c
@@ -1,6 +1,4 @@
 #include "shell.h"
-#include <stdlib.h>
-
 /**
  * main - entry point of the program
  * Description: run the shell program
@@ -19,26 +17,23 @@ int main(void)
 	{
 		printPrompt(); /* Print the prompt */
 		read = getline(&input_line, &len, stdin);
-
 		if (read == -1) /* Check for EOF or error */
 			break;
-
 		/* Check if emptyLine is filled with spaces or tabs */
 		if (emptyLine(input_line) == 1) /* 1 is true */
 			continue; /* Go back to the beginning of the loop (printPrompt again) */
+
 		/* Parse the input line and store them in command_arguments array*/
 		command_arguments = parseLine(input_line);
+
 		/* Check if the user wants to exit the shell */
-		if (strcmp(command_arguments[0], "exit") == 0)
+		if (strcmp(command_arguments[0], "exit") == 0) /* compare if arg0 == exit */
 		{
-			/* Free allocated memory */
-			freeDP(command_arguments);
-			free(input_line);
-			/* If an argument is provided, use it as the exit status */
-			if (command_arguments[1] != NULL)
-				exit(atoi(command_arguments[1])); /* Convert argument to integer */
-			exit(0); /* Exit with status 0 if no argument provided */
+			freeDP(command_arguments); /* Free the command_arguments array */
+			free(input_line);         /* Free the input_line */
+			return (0);               /* Return 0 on success */
 		}
+
 		/* Check if the user input is not a built-in command */
 		if (execute(command_arguments) == -1)
 		{
@@ -49,5 +44,5 @@ int main(void)
 		freeDP(command_arguments); /* Free the command_arguments array */
 	}
 	free(input_line); /* Free the input_line */
-	return (0);         /* Return 0 on success */
+	return (0);       /* Return 0 on success */
 }
