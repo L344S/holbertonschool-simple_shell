defd10efc7905cebeec6714aebeaa5f41f154234
yazgahar <battianas@hotmail.com>
2023-12-19 23:17:48 +0100

    handle exit output

--- execute.c
+++ execute.c
@@ -5,45 +5,54 @@
  * Description: compares the command with the built-in commands and executes
  * Return: 0 in success or EXIT_FAILURE
  */
-int execute(char **args)
-{
-	char *path_cmd = NULL;
-	pid_t child_pid;
-	int child_status;
-	char **original_environ = environ; /** Save the original value of environ */
+int execute(char **args) {
+    char *path_cmd = NULL;
+    pid_t child_pid;
+    int child_status;
+    char **original_environ = environ; /** Save the original value of environ */
 
-	if (strcmp(args[0], "env") == 0)
-	{
-		while (*environ)
-			printf("%s\n", *environ++); /** Print the environment */
-		environ = original_environ; /** Restore the original value of environ */
-		return (0);
-	}
-	if (strcmp(args[0], "cd") == 0)
-	{
-		if (args[1] == NULL)
-			chdir(getenv("HOME"));
-		else if (chdir(args[1]) != 0)
-			perror("cd");
-		return (0);
-	}
-	path_cmd = getPath(args[0]);
-	if (path_cmd == NULL)
-		return (-1);
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("Error: fork failed");
-		free(path_cmd);
-		return (-1);
-	} else if (child_pid == 0)
-	{
-		environ = original_environ; /** Restore ... in the child process */
-		if (execve(path_cmd, args, environ) == -1)
-			perror("Error: execve failed");
-		exit(EXIT_FAILURE);
-	} else
-		waitpid(child_pid, &child_status, 0);
-	free(path_cmd);
-	return (0);
+    if (strcmp(args[0], "env") == 0) {
+        while (*environ)
+            printf("%s\n", *environ++); /** Print the environment */
+        environ = original_environ; /** Restore the original value of environ */
+        return (0);
+    }
+
+    if (strcmp(args[0], "cd") == 0) {
+        if (args[1] == NULL)
+            chdir(getenv("HOME"));
+        else if (chdir(args[1]) != 0)
+            perror("cd");
+        return (0);
+    }
+
+    path_cmd = getPath(args[0]);
+    if (path_cmd == NULL)
+        return (-1);
+
+    child_pid = fork();
+    if (child_pid == -1) {
+        perror("Error: fork failed");
+        free(path_cmd);
+        return (-1);
+    } else if (child_pid == 0) {
+        environ = original_environ; /** Restore ... in the child process */
+        if (execve(path_cmd, args, environ) == -1) {
+            perror("Error: execve failed");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        waitpid(child_pid, &child_status, 0);
+
+        free(path_cmd);
+
+        if (WIFEXITED(child_status)) {
+            return WEXITSTATUS(child_status);
+        } else {
+            /** Handle the case where the child process did not terminate normally */
+            fprintf(stderr, "Child process did not terminate normally.\n");
+            return -1; /** Or another appropriate error code */
+        }
+    }
+	return(0);
 }
--- shell.c
+++ shell.c
@@ -6,38 +6,45 @@
  * Print the prompt, get the input, parse the input line, execute the command
  * Return: 0 on success or a number on failure
  */
-int main(void)
-{
+int main(void) {
     char *input_line = NULL;
     char **command_arguments;
     size_t len = 0;
     ssize_t read;
-	
-	while (1)
-    {
+    int exit_status;
+
+    while (1) {
         printPrompt(); /* Print the prompt */
         read = getline(&input_line, &len, stdin);
+        
         if (read == -1) /* Check for EOF or error */
             break;
+
         /* Check if emptyLine is filled with spaces or tabs */
         if (emptyLine(input_line) == 1) /* 1 is true */
-            continue; /* Go back to the beginning of the loop (printPrompt again) */        /* Parse the input line and store them in command_arguments array*/
-        command_arguments = parseLine(input_line);        /* Check if the user wants to exit the shell */
-        if (strcmp(command_arguments[0], "exit") == 0) /* compare if arg0 == exit */
-        {
+            continue; /* Go back to the beginning of the loop (printPrompt again) */
+
+        /* Parse the input line and store them in command_arguments array */
+        command_arguments = parseLine(input_line);
+
+        /* Check if the user wants to exit the shell */
+        if (strcmp(command_arguments[0], "exit") == 0) {
             freeDP(command_arguments); /* Free the command_arguments array */
             free(input_line);         /* Free the input_line */
             exit(0);               /* Return 0 on success */
-        }        /* Check if the user input is not a built-in command */
-        if (execute(command_arguments) == -1)
-        {
+        }
+
+        /* Check if the user input is not a built-in command */
+        exit_status = execute(command_arguments);
+
+        if (exit_status == -1) {
             /* Print error message */
             fprintf(stderr, "hsh: command not found: %s\n", command_arguments[0]);
-			freeDP(command_arguments); /* Free the command_arguments array */
-            continue; /* Go back to the beginning of the loop (printPrompt again) */
         }
+
         freeDP(command_arguments); /* Free the command_arguments array */
     }
+
     free(input_line); /* Free the input_line */
-    return (0);       /* Return 0 on success */
+    return exit_status; /* Return the exit status */
 }
