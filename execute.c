e2c471fa889e3b91c8c4e15e84fab6a09bb22ad6
yazgahar <battianas@hotmail.com>
2023-12-19 13:39:06 +0100

    modified env

--- execute.c
+++ execute.c
@@ -3,18 +3,20 @@
  * execute - execute the command
  * @args: array of arguments -> command and arguments
  * Description: compares the command with the built-in commands and executes
- * Return: 0 in success or EXIT_FAILIURE
+ * Return: 0 in success or EXIT_FAILURE
  */
 int execute(char **args)
 {
 	char *path_cmd = NULL;
 	pid_t child_pid;
 	int child_status;
+	char **original_environ = environ; /** Save the original value of environ */
 
 	if (strcmp(args[0], "env") == 0)
 	{
 		while (*environ)
-			printf("%s\n", *environ++);
+			printf("%s\n", *environ++); /** Print the environment */
+		environ = original_environ; /** Restore the original value of environ */
 		return (0);
 	}
 	if (strcmp(args[0], "cd") == 0)
@@ -31,14 +33,15 @@ int execute(char **args)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-		perror("Error ; fork failed");
+		perror("Error: fork failed");
 		free(path_cmd);
 		return (-1);
 	}
 	else if (child_pid == 0)
 	{
+		environ = original_environ; /** Restore ... in the child process */
 		if (execve(path_cmd, args, environ) == -1)
-			perror("Error ; execve failed");
+			perror("Error: execve failed");
 		exit(EXIT_FAILURE);
 	}
 	else
