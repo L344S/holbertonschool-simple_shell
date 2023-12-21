8cc25f9152ca26266108c6bbe0c6834cb63f372a
L344S <7474@holbertonstudents.com>
2023-12-21 02:22:04 +0100

    Updated Shell.c (main), Added comments + Betty Check

--- shell.c
+++ shell.c
@@ -8,51 +8,45 @@
 */
 int main(void)
 {
-	char *input_line = NULL;
+	char *input_line = NULL; /* stores the input line by the user */
+	char **command_arguments; /* stores the parsed commannd arguments */
+	size_t len = 0; /* stores the length of the input line */
+	ssize_t read; /* stores the result of getline (number of characters read) */
 
-	char **command_arguments;
-
-	size_t len = 0;
-	ssize_t read;
-
-	while (1)
+	while (1) /* Infinite loop */
 	{
-		printPrompt(); /* Print the prompt */
-		read = getline(&input_line, &len, stdin);
+		printPrompt(); /* Calls a function to print the shell prompt. */
+		read = getline(&input_line, &len, stdin); /* Read the input line */
 		if (read == -1) /* Check for EOF or error */
-			break;
-		/* Check if emptyLine is filled with spaces or tabs */
-		if (emptyLine(input_line) == 1) /* 1 is true */
+			break; /* If yes : break out of the loop */
+		if (emptyLine(input_line) == 1) /*Check if i_l contains only spaces or tabs*/
 			continue;
-		command_arguments = parseLine(input_line);
-		if (strcmp(command_arguments[0], "exit") == 0) /* compare if arg0 == exit */
+		command_arguments = parseLine(input_line); /* i_l - array command argument */
+		if (strcmp(command_arguments[0], "exit") == 0) /* Handle "exit" command */
 		{
-			if (command_arguments[1] == NULL)
+			if (command_arguments[1] == NULL) /* Check if there is an argu after exit */
 			{
-			freeDP(command_arguments); /* Free the command_arguments array */
+			freeDP(command_arguments); /* Free memory the command_arguments array */
 			free(input_line); /* Free the input_line */
-			exit(0); /* Return 0 on success */
+			exit(0); /* Exit with status 0 */
 			}
 			else
 			{
-				freeDP(command_arguments); /* Free the command_arguments array */
+				freeDP(command_arguments); /* Free memory the command_arguments array */
 				free(input_line); /* Free the input_line */
-				exit(2); /* Return 2 if there is an argument after exit */
+				exit(2); /* Exit with status 2 if there is an argu after exit */
 				}
-		/* Return 0 on success */
-		}        /* Check if the user input is not a built-in command */
-		if (execute(command_arguments) == -1)
+		}
+		if (execute(command_arguments) == -1) /* Check if the command is not found */
 		{
-			/* Print error message */
 			fprintf(stderr, "./hsh: 1: %s: not found\n", command_arguments[0]);
 			freeDP(command_arguments); /* Free the command_arguments array */
-			free(input_line);
-			exit(127); /* Return 127 on failure */
+			free(input_line); /* Free the input_line */
+			exit(127); /* Exit with status 127 */
 			continue; /* Go back to the beginning of the loop (printPrompt again) */
 		}
-		freeDP(command_arguments); /* Free the command_arguments array */
-	}
-	free(input_line); /* Free the input_line */
-	return (0); /* Return 0 on success */
+		freeDP(command_arguments); /* Free memory of command_arguments array */
+	} /* End of while loop */
+	free(input_line); /* Free the memory of input_line */
+	return (0); /* return 0 on success */
 }
-
