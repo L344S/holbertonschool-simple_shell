9c2f6c5ffa5a6b2f6e12a1820d2060d660784c13
yazgahar <battianas@hotmail.com>
2023-12-21 23:13:44 +0100

    try to get the last point

--- execute.c
+++ execute.c
@@ -6,53 +6,39 @@
  * Description: compares the command with the built-in commands and executes
  * Return: 0 in success or EXIT_FAILURE
  */
-int execute(char **args)
-{
-	char *path_cmd = NULL; /* Stores the path of the command */
-	pid_t child_pid; /* Stores the pid of the child process */
-	int child_status; /* Stores the status of the child process */
-	char **original_environ = environ; /* Stores the original value of environ */
 
-	if (strcmp(args[0], "env") == 0) /* Handle "env" command */
-	{
-		while (*environ) /* Loop through the environment */
-			printf("%s\n", *environ++); /* Print the environment and increment */
-		environ = original_environ; /* Restore the original value of environ */
-		return (0); /* Return 0 on success */
-	}
-	if (strcmp(args[0], "cd") == 0) /* Handle "cd" command */
-	{
-		if (args[1] == NULL) /* If there is no argument after cd */
-			chdir(getenv("HOME")); /* Change the current directory to HOME */
-		else if (chdir(args[1]) != 0) /* If chdir fails */
-			perror("cd"); /* Print a error message */
-		return (0); /* Return 0 on success */
-	}
+int execute(char **command_arguments) {
+    pid_t child_pid;
+    int status;
 
-	path_cmd = getPath(args[0]); /* Get the path of the command */
-	if (path_cmd == NULL) /* If the command is not found */
-		return (-1); /* Return -1, indicating failure */
+    child_pid = fork();
 
-	child_pid = fork(); /* Create a child process */
-	if (child_pid == -1) /* If fork fails */
-	{
-		perror("Error: fork failed"); /* Print a error message */
-		free(path_cmd); /* Free the path_cmd */
-		return (-1); /* Return -1, indicating failure */
-	}
-	if (child_pid == 0) /* If it's the child process */
-	{
-		environ = original_environ; /* Restore the original value of environ */
-		if (execve(path_cmd, args, environ) == -1) /* Execute the command but if it fails */
-		{
-			perror("Error: execve failed"); /* Print a error message */
-			free(path_cmd); /* Free the path_cmd */
-			exit(EXIT_FAILURE); /* Exit with status EXIT_FAILURE */
-		}
-		else /* If execve succeeds */
-			waitpid(child_pid, &child_status, 0); /* Wait for the child process */
-	}
+    if (child_pid == -1) {
+        perror("fork");
+        return -1; /* Indicates an error */
+    }
 
-	free(path_cmd); /* Free the path_cmd */
-	return (0); /* Return 0 on success */
+    if (child_pid == 0) {
+        /* Code for the child process */
+        execvp(command_arguments[0], command_arguments);
+        perror(command_arguments[0]); /* execvp failed */
+        exit(2); /* Exit with status 2 if execvp fails */
+    } else {
+        /* Code for the parent process */
+        waitpid(child_pid, &status, 0);
+
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+
+            if (exit_status == 2) {
+                fprintf(stderr, "./hsh: 1: %s: No such file or directory\n", command_arguments[0]);
+                return -1; /* Indicates command not found */
+            }
+        } else {
+            perror("waitpid");
+            return -1; /* Indicates an error */
+        }
+    }
+
+    return 0; /* Indicates success */
 }
--- shell.c
+++ shell.c
@@ -9,46 +9,48 @@
  */
 int main(void)
 {
-	char *input_line = NULL;         /* stores the input line by the user */
-	char **command_arguments = NULL; /* stores the parsed command arguments */
-	size_t len = 0;                   /* stores the length of the input line */
-	ssize_t read;	/* stores the result of getline (number of characters read) */
+    char *input_line = NULL;         /* stores the input line by the user */
+    char **command_arguments = NULL; /* stores the parsed command arguments */
+    size_t len = 0;                   /* stores the length of the input line */
+    ssize_t read;	/* stores the result of getline (number of characters read) */
 
-	while (1) /* Infinite loop */
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			printPrompt(); /* Calls a function to print the shell prompt. */
-			fflush(stdout);
-		}
-		read = getline(&input_line, &len, stdin); /* Read the input line */
+    while (1) /* Infinite loop */
+    {
+        if (isatty(STDIN_FILENO))
+        {
+            printPrompt(); /* Calls a function to print the shell prompt. */
+            fflush(stdout);
+        }
 
-		if (emptyLine(input_line) == 1)
-			continue;
+        read = getline(&input_line, &len, stdin); /* Read the input line */
 
-		if (read == EOF)
-		{
-			free(input_line); /* Free the input_line */
-			exit(0);          /* Exit with status 0 */
-		}
+        if (read == EOF)
+        {
+            free(input_line); /* Free the input_line */
+            exit(0);          /* Exit with status 0 */
+        }
 
-		command_arguments = parseLine(input_line); /* i_l - array command argument */
+        command_arguments = parseLine(input_line); /* i_l - array command argument */
 
-		if (strcmp(command_arguments[0], "exit") == 0) /* Handle "exit" command */
-		{
-			freeDP(command_arguments); /* Free memory of command_arguments array */
-			free(input_line);         /* Free the input_line */
-			exit(0);                  /* Exit with status 0 */
-		}
+        if (strcmp(command_arguments[0], "exit") == 0) /* Handle "exit" command */
+        {
+            freeDP(command_arguments); /* Free memory of command_arguments array */
+            free(input_line);         /* Free the input_line */
+            exit(0);                  /* Exit with status 0 */
+        }
 
-		if (execute(command_arguments) == -1) /* Check if the command is not found */
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", command_arguments[0]);
-		}
+        if (emptyLine(input_line) == 1)
+            continue;
 
-		freeDP(command_arguments); /* Free memory of command_arguments array */
-	}                              /* End of while loop */
+        if (execute(command_arguments) == -1)
+        {
+            free(input_line); /* Free the input_line */
+            exit(2);          /* Exit with status 2 */
+        }
 
-	free(input_line); /* Free the memory of input_line */
-	return (0);         /* return 0 on success */
+        freeDP(command_arguments); /* Free memory of command_arguments array */
+    }                              /* End of while loop */
+
+    free(input_line); /* Free the memory of input_line */
+    return 0;         /* return 0 on success */
 }
