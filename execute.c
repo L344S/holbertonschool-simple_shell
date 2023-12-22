9a74114c772c47778bd57b3cc04ffcfcaeb63be0
yazgahar <battianas@hotmail.com>
2023-12-22 17:15:13 +0100

    try to get the last point

--- execute.c
+++ execute.c
@@ -1,49 +1,43 @@
 #include "shell.h"
+
 /**
  * execute - execute the command
  * @args: array of arguments -> command and arguments
  * Description: compares the command with the built-in commands and executes
  * Return: 0 in success or EXIT_FAILURE
  */
-int execute(char **args)
-{
-	char *path_cmd = NULL;
-	pid_t child_pid;
-	int child_status;
-	char **original_environ = environ; /** Save the original value of environ */
 
-	if (strcmp(args[0], "env") == 0)
-	{
-		while (*environ)
-			printf("%s\n", *environ++); /** Print the environment */
-		environ = original_environ; /** Restore the original value of environ */
-		return (0);
-	}
-	if (strcmp(args[0], "cd") == 0)
-	{
-		if (args[1] == NULL)
-			chdir(getenv("HOME"));
-		else if (chdir(args[1]) != 0)
-			perror("cd");
-		return (0);
-	}
-	path_cmd = getPath(args[0]);
-	if (path_cmd == NULL)
-		return (-1);
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("Error: fork failed");
-		free(path_cmd);
-		return (-1);
-	} else if (child_pid == 0)
-	{
-		environ = original_environ; /** Restore ... in the child process */
-		if (execve(path_cmd, args, environ) == -1)
-			perror("Error: execve failed");
-		exit(EXIT_FAILURE);
-	} else
-		waitpid(child_pid, &child_status, 0);
-	free(path_cmd);
-	return (0);
+int execute(char **command_arguments) {
+    pid_t child_pid;
+    int status;
+
+    child_pid = fork();
+
+    if (child_pid == -1) {
+        perror("fork");
+        return -1; /* Indicates an error */
+    }
+
+    if (child_pid == 0) {
+        /* Code for the child process */
+        execvp(command_arguments[0], command_arguments);
+        perror(command_arguments[0]); /* execvp failed */
+        exit(2); /* Exit with status 2 if execvp fails */
+    } else {
+        /* Code for the parent process */
+        waitpid(child_pid, &status, 0);
+
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+
+            if (exit_status == 2) {
+                return -1; /* Indicates command not found */
+            }
+        } else {
+            perror("waitpid");
+            return -1; /* Indicates an error */
+        }
+    }
+
+    return 0; /* Indicates success */
 }
